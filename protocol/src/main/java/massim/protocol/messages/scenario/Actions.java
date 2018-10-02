839eaf28600b7b68c3edbe782d606d74d645f492
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2018-10-02 16:41:20 +0200

    replace with factory method

--- protocol/src/main/java/massim/protocol/messages/scenario/Actions.java
+++ protocol/src/main/java/massim/protocol/messages/scenario/Actions.java
@@ -1,12 +1,10 @@
 package massim.protocol.messages.scenario;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public abstract class Actions {
 
     public final static String GOTO = "goto";
 
-    public static final List<String> ALL_ACTIONS = new ArrayList<>(Arrays.asList(GOTO));
+    public static final List<String> ALL_ACTIONS = List.of(GOTO);
 }
