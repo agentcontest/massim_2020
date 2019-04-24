acf12451004cc75f746172c345782b23a460f693
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-04-24 18:51:46 +0200

    prepare canvas

--- monitor/js/main.ts
+++ monitor/js/main.ts
@@ -1,1 +1,5 @@
-console.log('Hello monitor!');
+export default function Monitor(canvas: HTMLCanvasElement) {
+  const ctx = canvas.getContext('2d')!;
+  ctx.fillStyle = 'green';
+  ctx.fillRect(10, 10, 10, 10);
+}
