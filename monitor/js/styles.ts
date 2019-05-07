dfd5bb69f037ac017aaf6817471a887b3396f6a3
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-05-07 15:34:33 +0200

    forgot to commit styles.ts

---
+++ monitor/js/styles.ts
@@ -0,0 +1,8 @@
+export const teams = ['blue', 'green'];
+
+export const goalStroke = 'red';
+export const goalFill = 'rgba(255, 0, 0, 0.4)';
+
+export const obstacle = '#333';
+
+export const blocks = ['#41470b', '#78730d', '#bab217', '#e3d682', '#b3a06f', '#9c7640', '#5a4c35'];
