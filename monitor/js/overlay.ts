756e33c1cac320c0ec491fab0bc66f302e9a67a1
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-05-07 13:10:12 +0200

    render team names

--- monitor/js/overlay.ts
+++ monitor/js/overlay.ts
@@ -1,7 +1,11 @@
-import { Ctrl } from './interfaces';
+import { Ctrl, StaticWorld } from './interfaces';
 
 import { h } from 'snabbdom';
 import { VNode } from 'snabbdom/vnode';
+
+function teams(world: StaticWorld): string {
+  return `${world.teams['A'].name} : ${world.teams['B'].name}`;
+}
 
 function disconnected(_ctrl: Ctrl): VNode {
   // TODO: Replay available?
@@ -15,11 +19,16 @@ function disconnected(_ctrl: Ctrl): VNode {
 
 export default function(ctrl: Ctrl): VNode {
   if (ctrl.vm.state === 'error') return disconnected(ctrl);
-  if (ctrl.vm.state === 'connecting' || !ctrl.vm.dynamic)
+  if (ctrl.vm.state === 'connecting' || !ctrl.vm.static || !ctrl.vm.dynamic)
     return h('div.box', [
       h('div.loader', 'Loading ...')
     ]);
-  return h('div.box', [
-    'Connected.'
+  return h('div#overlay', [
+    h('div.box', [
+      'Connected.'
+    ]),
+    h('div.box', [
+      teams(ctrl.vm.static)
+    ])
   ]);
 }
