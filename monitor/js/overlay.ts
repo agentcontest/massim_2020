7458adb6a3f055727e602d235f120d66b5f15d9e
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-05-07 13:14:19 +0200

    render list of tasks

--- monitor/js/overlay.ts
+++ monitor/js/overlay.ts
@@ -1,10 +1,16 @@
-import { Ctrl, StaticWorld } from './interfaces';
+import { Ctrl, StaticWorld, DynamicWorld } from './interfaces';
 
 import { h } from 'snabbdom';
 import { VNode } from 'snabbdom/vnode';
 
 function teams(world: StaticWorld): string {
   return `${world.teams['A'].name} : ${world.teams['B'].name}`;
+}
+
+function tasks(world: DynamicWorld): VNode {
+  return h('ul', world.tasks.map(t => {
+    return h('li', `${t.reward}$$ for ${t.name} until ${t.deadline}`);
+  }));
 }
 
 function disconnected(_ctrl: Ctrl): VNode {
@@ -27,8 +33,7 @@ export default function(ctrl: Ctrl): VNode {
     h('div.box', [
       'Connected.'
     ]),
-    h('div.box', [
-      teams(ctrl.vm.static)
-    ])
+    h('div.box', teams(ctrl.vm.static)),
+    h('div.box', tasks(ctrl.vm.dynamic))
   ]);
 }
