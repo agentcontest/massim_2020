348ff57c72c73f4ac48b61d95df113012c8b5c24
Tobias Ahlbrecht <ta10@tu-clausthal.de>
2019-07-18 12:06:53 +0200

    remove unused const

--- server/src/main/java/massim/simulation/game/environment/Block.java
+++ server/src/main/java/massim/simulation/game/environment/Block.java
@@ -5,8 +5,6 @@ import massim.protocol.data.Thing;
 
 public class Block extends Attachable {
 
-    public final static String BLOCK_TYPE_NORMAL = "normal";
-
     private String blockType;
 
     public Block(Position xy, String blockType) {
