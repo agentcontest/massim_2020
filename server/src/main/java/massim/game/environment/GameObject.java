22f46a685f4d6d864a425129ac5f654c18d8f9aa
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-08-20 11:01:40 +0200

    make game object ids more compact

--- server/src/main/java/massim/game/environment/GameObject.java
+++ server/src/main/java/massim/game/environment/GameObject.java
@@ -4,12 +4,15 @@ import massim.protocol.data.Position;
 import massim.protocol.data.Thing;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class GameObject {
 
-    private String id = UUID.randomUUID().toString();
+    private static AtomicInteger nextId = new AtomicInteger(1);
 
-    public String getID() {
+    private final String id = Integer.toString(nextId.getAndIncrement());
+
+    public final String getID() {
         return id;
     }
 
