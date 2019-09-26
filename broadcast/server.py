8f29f91096b44679037c273cbb7d42e3efe7ecea
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-09-26 12:36:44 +0200

    broadcast: load entire replay

--- broadcast/server.py
+++ broadcast/server.py
@@ -1,20 +1,40 @@
-import os
+import asyncio
 import aiohttp.web
-import sys
 import json
+import os
+import sys
 
 MONITOR_WWW = "../monitor/src/main/resources/www"
 
 class LiveBroadcast:
     def __init__(self, path):
+        self.path = path
+        self.dynamic = []
+        self.step = 0
+
         with open(os.path.join(path, "static.json")) as f:
             self.static = json.load(f)
+
+        for step in range(self.static["steps"]):
+            if step % 5 == 0:
+                with open(os.path.join(path, f"{step}.json")) as f:
+                    group = json.load(f)
+            self.dynamic.append(group[str(step)])
+
+    async def run(self):
+        for step in range(self.static["steps"]):
+            self.step = step
+            print(self.path, self.step, "/", self.static["steps"])
+            await asyncio.sleep(1)
 
     async def live(self, req):
         ws = aiohttp.web.WebSocketResponse()
         await ws.prepare(req)
 
         await ws.send_json(self.static)
+
+        if self.dynamic is not None:
+            await ws.send_json(self.dynamic[self.step])
 
         return ws
 
@@ -23,14 +43,16 @@ def static(path):
         return aiohttp.web.FileResponse(os.path.join(os.path.dirname(__file__), path))
     return handler
 
-def main(path):
+async def main(path):
     broadcast = LiveBroadcast(path)
+
+    asyncio.create_task(broadcast.run())
 
     app = aiohttp.web.Application()
     app.router.add_route("GET", "/", static(os.path.join(MONITOR_WWW, "index.html")))
     app.router.add_route("GET", "/live/monitor", broadcast.live)
     app.router.add_static("/", MONITOR_WWW)
-    aiohttp.web.run_app(app)
+    return app
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    aiohttp.web.run_app(main(sys.argv[1]))
