fd7078d8ed75806354398d10c62e2b1d041c82ee
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-07-21 19:48:41 +0200

    briefly get monitor back to a working state

---
+++ monitor/js/view.ts
@@ -0,0 +1,13 @@
+import { h } from 'snabbdom/h';
+import { VNode } from 'snabbdom/vnode';
+
+import { Ctrl } from './interfaces';
+import { overlay } from './overlay';
+import { mapView } from './map';
+
+export function view(ctrl: Ctrl): VNode {
+  return h('div#monitor', [
+    overlay(ctrl),
+    //mapView(ctrl.map),
+  ]);
+}
